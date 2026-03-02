f786113562fdb6c6db578fb7b1ba820afa9a74df
ahmtkzk <ahmetkazak08@gmail.com>
2026-03-02 23:54:40 +0300

    Cookies allowed

--- src/main/java/com/noteder/be/config/SecurityConfig.java
+++ src/main/java/com/noteder/be/config/SecurityConfig.java
@@ -60,9 +60,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*")); // Allow all origins
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
